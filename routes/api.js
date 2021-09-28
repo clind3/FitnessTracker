4998fcac332f28bb53fc3eb21f9de1a2c2e1012e
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-09-27 21:11:27 -0400

    finish api routes and create totalDuration fcn for workouts

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,32 @@
 const express = require('express');
-const Workout = require('../models/workout');
+const db = require('../models/workout.js');
 const router = express.Router();
+
+totalDuration = (workoutsdata) => {
+const allData = [];
+workoutsdata.forEach(workout => {
+    let duration = 0;
+    let exData = JSON.stringify(workout);
+    exData = JSON.parse(exData);
+
+    (exData.exercises).forEach(element => {
+        duration += element.duration;
+        console.log(duration);
+    });
+
+    exData.totalDuration = duration;
+    console.log('EXDATA: '+ exData.totalDuration);
+    allData.push(exData);
+});
+return allData;
+}
 
 //get all workouts
 router.get('/workouts', (req, res, next) => {
-    Workout.find().sort({date: -1})
+    db.find().sort({date: -1})
     .then(workoutData => {
-        res.json(workoutData);
+        const workouts = totalDuration(workoutData);
+        res.status(200).json(workouts);
     })
     .catch((err) => {
         res.json(err);
@@ -14,3 +34,36 @@ router.get('/workouts', (req, res, next) => {
 }
 );
 
+//create workout
+router.post('/workouts', (req,res) => {
+    db.create({}).then(data => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+//find workouts within range on stats page
+router.get('/workouts/range', (req,res) => {
+    db.find()
+    .then(data => {
+        res.json(data);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
+
+//adding exercise to workout
+router.put("/workouts/:id", (req,res) => {
+    db.findByIdAndUpdate(req.params.id,
+        {$push: {exercises: req.body}}
+    )
+    .then(data => res.json(data))
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+
+module.exports = router;
